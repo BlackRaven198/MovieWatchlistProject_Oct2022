c21bd1f02e73d95fadcb18e22f80dd8aaf0bff88
BlackRaven198 <76513030+BlackRaven198@users.noreply.github.com>
2022-10-10 00:52:32 -0700

    Updates
    Continue to make changes

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ searchBar.addEventListener('keyup', function (e){
         console.log('enter key')
         input = document.getElementById('search-bar').value
         console.log(input)
-        fetch(`http://www.omdbapi.com/?t=${ input }&apikey=da1b8236`)
+        fetch(`http://www.omdbapi.com/?s=${ input }&apikey=da1b8236`)
             .then(res => res.json())
-            .then(data => handleData(data) & console.log('hello again') & printMovieData(data))
+            .then(data => handleData(data) & console.log('hello again') & printMovieDataTwo(data))
     }
 })
 // Search Button click and send a pull request to the API  
@@ -38,9 +38,9 @@ searchBtn.addEventListener('click', () => {
     console.log('click')
     input = document.getElementById('search-bar').value
     console.log(input)
-    fetch(`http://www.omdbapi.com/?t=${ input }&apikey=da1b8236`)
+    fetch(`http://www.omdbapi.com/?s=${ input }&apikey=da1b8236`)
         .then(res => res.json())
-        .then(data => handleData(data) & printMovieData(data))
+        .then(data => handleData(data) & printMovieDataTwo(data))
 })
 //callback function that runs inside of search click  
 let handleData = (data) => { console.log(data.title)}
@@ -51,6 +51,8 @@ let handleData = (data) => { console.log(data.title)}
 
 
 //Function to create the body and container div with movie data
+//this only works for one result back using the "t" parameter ex.(t={title}) becasue it takes the data then directly acces the poster, title, ratings, and so on. Need to convert so the app shows the first 10 hits on the page. 
+//Perhaps on the next build load all sites however create multiple pages for it. 
 let printMovieData = (data) => { 
     console.log('print movie data')
     console.log(data)
@@ -85,6 +87,46 @@ let printMovieData = (data) => {
     `
 
 }
-// printMovieData()
 
-//   <img id="poster-img" src="${data.poster}">
+//Second itteration of the printMovieData function
+let printMovieDataTwo = (data) => { 
+    console.log('print movie data')
+    console.log(data)
+    document.getElementById('movie-data').innerHTML = `
+        <div class="container-movie-card"> 
+            <div class="movie-card"> 
+
+                <div class="card-left">
+                    <img id="card-left-img" src="${data.Search[0].Poster}">
+                </div>
+
+                <div class="card-right">
+                    <div class="card-title">
+                         <h3 id="movie-card-title"> ${data.Search[0].Title}</h3>
+                         
+                    </div>
+                    
+                    <div class="card-runtime-genre-add">
+                        <p id="card-runtime"> ${data.Search[0].Runtime} </p>
+                        <p> ${data.Search[0].Genre} </p>
+                        <a href="watchlist.html">➕Watchlist</a>
+                    </div>
+                    <div id="description"> 
+                        <p> ${data.Search[0].Plot} </p>
+                    </div>
+                </div>
+            
+
+            </div>
+            <div id="border-bottom"></div>
+        </div>
+    `
+
+}
+//Code that belongs in printMovieDataTwo function 
+//<p id="rating"> ${data.Search[0].Ratings[0].Value}</p>
+
+
+
+
+
